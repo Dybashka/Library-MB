d39ee94ac68de8177cd91f2edcac1eaabad0db4a
Dybashka <72248943+Dybashka@users.noreply.github.com>
2022-12-07 15:26:00 +0300

    ver1
    реализовали:
    создание бд
    подключение к бд
    2 функции работы с бд

---
+++ Ver_PK/create_bd.py
@@ -0,0 +1,82 @@
+import psycopg2
+from psycopg2 import Error
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+user = input("Введите имя пользователя: ")
+password = input("Введите пароль: ")
+try:
+    # Подключение к существующей базе данных
+    connection = psycopg2.connect(user=user,
+                                  password=password,
+                                  host="127.0.0.1",
+                                  port="5432")
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    # Курсор для выполнения операций с базой данных
+    cursor = connection.cursor()
+    sql_create_database = '''CREATE database LibraryMB;
+                              CREATE TABLE books (
+                                    book_id SERIAL PRIMARY KEY,
+                                    title VARCHAR(50) NOT NULL,
+                                    author VARCHAR(50) NOT NULL,
+                                    publ_house VARCHAR(50) NOT NULL,
+                                    price NUMERIC(9, 2) NOT NULL,
+                                    pages_cnt INT NOT NULL,
+                                    pictures_cnt INT,
+                                    book_year INT NOT NULL
+                                );
+
+                                CREATE TABLE students (
+                                    student_id SERIAL PRIMARY KEY,
+                                    student_fname VARCHAR(30) NOT NULL,
+                                    student_lname VARCHAR(30) NOT NULL,
+                                    student_mname VARCHAR(30)
+                                );
+
+                                CREATE TABLE branches (
+                                    br_name VARCHAR(50) PRIMARY KEY
+                                );
+
+                                CREATE TABLE departments (
+                                    dp_name VARCHAR(50) PRIMARY KEY
+                                );
+
+                                CREATE TABLE issuance (
+                                    book_id INT NOT NULL,
+                                    student_id INT NOT NULL,
+                                    PRIMARY KEY (book_id, student_id),
+                                    FOREIGN KEY (book_id) REFERENCES books (book_id),
+                                    FOREIGN KEY (student_id) REFERENCES students (student_id)
+                                );
+
+                                CREATE TABLE storing (
+                                    br_name VARCHAR(50) NOT NULL,
+                                    book_id INT NOT NULL,
+                                    PRIMARY KEY(br_name, book_id),
+                                    book_cnt INT NOT NULL,
+                                    FOREIGN KEY (br_name) REFERENCES branches (br_name),
+                                    FOREIGN KEY (book_id) REFERENCES books (book_id)
+                                );
+
+                                CREATE TABLE use (
+                                    dp_name VARCHAR(50) NOT NULL,
+                                    book_id INT NOT NULL,
+                                    PRIMARY KEY(dp_name, book_id),
+                                    FOREIGN KEY (dp_name) REFERENCES departments (dp_name),
+                                    FOREIGN KEY (book_id) REFERENCES books (book_id)
+                                );
+
+                                CREATE TABLE belonging (
+                                    br_name VARCHAR(50) NOT NULL,
+                                    dp_name VARCHAR(50) NOT NULL,
+                                    PRIMARY KEY(br_name, dp_name),
+                                    FOREIGN KEY (br_name) REFERENCES branches (br_name),
+                                    FOREIGN KEY (dp_name) REFERENCES departments (dp_name)
+                                );'''
+    cursor.execute(sql_create_database)
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
---
+++ Ver_PK/main.py
@@ -0,0 +1,69 @@
+import psycopg2
+import time
+from psycopg2 import Error
+
+user = input("Введите имя пользователя: ")
+password = input("Введите пароль: ")
+try:
+    # Подключение к существующей базе данных
+    connection = psycopg2.connect(user=user,
+                                  password=password,
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="librarymb")
+
+    # Курсор для выполнения операций с базой данных
+    cursor = connection.cursor()
+    # Выполнение SQL-запроса
+    cursor.execute("SELECT version();")
+    # Получить результат
+    record = cursor.fetchone()
+    print("Вы подключены к - ", record, "\n")
+    msg = """
+    Выполнить:
+    0. Завершение работы
+    1. Добавление информации о книгах в библиотеку
+    2. Изменить информацию о книге в библиотеке
+    3. Посмотреть информацию о всех книгах в библиотеке
+    4. Добавить информацию о филиале
+    5. Изменить информацию о филиале
+    6. Для указанного филиала посчитать количество экземпляров указанной книги, находящихся в нем
+    7. Для указанной книги посчитать количество факультетов, на которых она используется в данном филиале, и вывести названия этих факультетов
+    """
+    n = int(input(msg))
+    while n != 0:
+        if n == 1:
+            values = tuple(input("Введите через пробел: название филиала, id киги, кол-во книг\n").split())
+            q = "INSERT INTO storing(br_name, book_id, book_cnt) VALUES (%s, %s, %s);"
+            cursor.execute(q, values)
+            connection.commit()
+        if n == 2:
+            print("non")
+        if n == 3:
+            q = "SELECT * FROM storing;"
+            cursor.execute(q)
+            mobile_records = cursor.fetchall()
+            print("Вывод каждой строки и ее столбцов")
+            for row in mobile_records:
+                print("название филиала =", row[0], )
+                print("Id книги =", row[1])
+                print("Количество книг =", row[2], "\n")
+            connection.commit()
+        if n == 4:
+            print("non")
+        if n == 5:
+            print("non")
+        if n == 6:
+            print("non")
+        if n == 7:
+            print("non")
+        n = int(input(msg))
+
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+else:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
+        time.sleep(3)
