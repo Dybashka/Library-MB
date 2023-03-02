2b49513ae5b913e964414aa52e52e34ab906ce79
VickyMosh <71924923+VickyMosh@users.noreply.github.com>
2023-03-02 12:49:16 +0300

    Update main.py
    изменения в коде для 7ой лабораторной

--- Ver_PK/main.py
+++ Ver_PK/main.py
@@ -2,14 +2,64 @@ import psycopg2
 import time
 from psycopg2 import Error
 
-print("ДОБро пожаловать ")
-user = input("Введите имя пользователя: ")
-password = input("Введите пароль: ")
+def choice(n):
+    while n != 0:
+        if n == 1:
+            values = tuple(input("Введите через пробел: название филиала, id книги, кол-во книг\n").split())
+            q = "INSERT INTO storing(br_name, book_id, book_cnt) VALUES (%s, %s, %s);"
+            cursor.execute(q, values)
+            connection.commit()
+            return 'Success'
+        elif n == 2:
+            values = tuple(input("Введите через пробел: название филиала, id книги, кол-во книг\n").split())
+            q = "UPDATE br_name SET book_cnt = 150 WHERE book_id = 2" #UPDATE books SET book_year = 2019 WHERE book_id = 5
+            cursor.execute(q, values)
+            connection.commit()
+            print("non")
+        elif n == 3:
+            q = "SELECT * FROM storing;"
+            cursor.execute(q)
+            mobile_records = cursor.fetchall()
+            print("Вывод каждой строки и ее столбцов")
+            for row in mobile_records:
+                print("название филиала =", row[0], )
+                print("Id книги =", row[1])
+                print("Количество книг =", row[2], "\n")
+            connection.commit()
+        elif n == 4:
+            q = "SELECT * FROM branches;"
+            cursor.execute(q)
+            mobile_records = cursor.fetchall()
+            for row in mobile_records:
+                print(row[0])
+            connection.commit()
+            return "Right"
+        elif n == 5:
+            print("non")
+        elif n == 6:
+            values = tuple(input("Введите id книги\n").split())
+            q = "SELECT book_cnt from storing where book_id = %s"
+            cursor.execute(q, values)
+            mobile_records = cursor.fetchall()
+            for row in mobile_records:
+                print("Количество книг =", row[0], "\n")
+            connection.commit()
+        elif n == 7:
+            print("non")
+        else:
+            return "Fail"
+        n = int(input("Введите действие: "))
+
+print('Welcome to Library-MB!')
+user = input("Введите имя пользователя: ") #postgres
+password = input("Введите пароль: ") #library
+
+
 try:
     # Подключение к существующей базе данных
     connection = psycopg2.connect(user=user,
                                   password=password,
-                                  host="db.local",
+                                  host="172.19.0.2",
                                   port="5432",
                                   database="libraryMB")
 
@@ -20,48 +70,21 @@ try:
     # Получить результат
     record = cursor.fetchone()
     print("Вы подключены к - ", record, "\n")
+
     msg = """
     Выполнить:
     0. Завершение работы
     1. Добавление информации о книгах в библиотеку
     2. Изменить информацию о книге в библиотеке
     3. Посмотреть информацию о всех книгах в библиотеке
-    4. Добавить информацию о филиале
-    5. Изменить информацию о филиале
-    6. Для указанного филиала посчитать количество экземпляров указанной книги, находящихся в нем
+    4. Просмотр филиалов
+    5  Добавить информацию о филиале
+    6. Посчитать количество экземпляров указанной книги
     7. Для указанной книги посчитать количество факультетов, на которых она используется в данном филиале, и вывести названия этих факультетов
     """
     n = int(input(msg))
-    while n != 0:
-        if n == 1:
-            values = list(input("Введите через пробел: название филиала, id киги, кол-во книг\n").split())
-            for i in range(len(values)):
-                values[i] = values[i].encode('utf-8', 'replace').decode('utf-8')
-            values = tuple(values)
-            q = "INSERT INTO storing(br_name, book_id, book_cnt) VALUES (%s, %s, %s);"
-            cursor.execute(q, values)
-            connection.commit()
-        if n == 2:
-            print("non")
-        if n == 3:
-            q = "SELECT * FROM storing;"
-            cursor.execute(q)
-            mobile_records = cursor.fetchall()
-            print("Вывод каждой строки и ее столбцов")
-            for row in mobile_records:
-                print("название филиала =", row[0], )
-                print("Id книги =", row[1])
-                print("Количество книг =", row[2], "\n")
-            connection.commit()
-        if n == 4:
-            print("non")
-        if n == 5:
-            print("non")
-        if n == 6:
-            print("non")
-        if n == 7:
-            print("non")
-        n = int(input(msg))
+    choice(n)
+
 
 except (Exception, Error) as error:
     print("Ошибка при работе с PostgreSQL", error)
