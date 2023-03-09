2796746102b281e5054c31fe09a4955002bce68b
Dybashka <72248943+Dybashka@users.noreply.github.com>
2023-03-09 18:23:48 +0300

    Update main.py

--- Ver_PK/main.py
+++ Ver_PK/main.py
@@ -9,7 +9,7 @@ try:
     # Подключение к существующей базе данных
     connection = psycopg2.connect(user=user,
                                   password=password,
-                                  host="db.local",
+                                  host="127.0.0.1",
                                   port="5432",
                                   database="libraryMB")
 
