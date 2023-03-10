754baa7dda28e3ef25139bab141c39d76fe2bab5
Dybashka <72248943+Dybashka@users.noreply.github.com>
2023-03-10 18:05:30 +0300

    Create my_fun.py
    add_fun

---
+++ Ver_PK/my_fun.py
@@ -0,0 +1,8 @@
+def retion_student_books(count_of_student, count_of_book):
+    ratio = round(count_of_student / count_of_book, 2)
+    return ratio
+
+
+def expenses(price, count_of_books):
+    expenses = price * count_of_books
+    return expenses
